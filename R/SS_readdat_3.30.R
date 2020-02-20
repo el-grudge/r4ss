45570ed43a49e51b84c622d72c9c9f3854c5bba6
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-02-20 15:41:28 -0800

    fix: reading of meanweight data when two lines

--- R/SS_readdat_3.30.R
+++ R/SS_readdat_3.30.R
@@ -457,30 +457,23 @@ SS_readdat_3.30 <-
   }
   if(d$use_MeanSize_at_Age_obs){
     ind.tmp <- ind # save current position in case necessary to re-read
-    d$MeanSize_at_Age_obs <- get.df(dat, ind)
-
-    # extra code in case sample sizes are on a separate line from other inputs
-    # first check if gender input is outside of normal range
-    if(!all(as.numeric(d$MeanSize_at_Age_obs$V4) %in% 0:3)){
+    endmwa <- ind - 2 + grep("-9999", dat[ind:length(dat)])[1]
+    xx <- dat[ind:endmwa]
+    if (length(unique(sapply(strsplit(xx, "\\s"), length))) > 1) {
       if(verbose){
         message("Format of MeanSize_at_Age_obs appears to have sample sizes",
                 "on separate lines than other inputs.")
       }
-      ind <- ind.tmp # reset index to value prior to first attempt to read table
-      N_MeanSize_at_Age_obs <- nrow(d$MeanSize_at_Age_obs)/2
-      MeanSize_at_Age_obs <- NULL
-      for(iobs in 1:N_MeanSize_at_Age_obs){
-        # each observation is a combination of pairs of adjacent rows
-        MeanSize_at_Age_obs <- rbind(MeanSize_at_Age_obs,
-                                     c(get.vec(dat,ind), get.vec(dat,ind)))
-      }
-      # check terminator row
+      xx <- paste(xx[1:length(xx) %% 2 == 1], xx[1:length(xx) %% 2 == 0])
+    }
+    d$MeanSize_at_Age_obs <- data.frame(do.call("rbind", strsplit(xx, "\\s+")),
+      stringsAsFactors = FALSE)
+    ind <- endmwa + 1
       test <- get.vec(dat,ind)
       if(test[1] != -9999){
         warning("Problem with read of MeanSize_at_Age, terminator value != -9999")
       }
-      d$MeanSize_at_Age_obs <- as.data.frame(MeanSize_at_Age_obs)
-    }
+
 
     colnames(d$MeanSize_at_Age_obs) <-
       c("Yr", "Seas", "FltSvy", "Gender", "Part", "AgeErr", "Ignore",
